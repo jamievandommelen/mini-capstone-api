531cd40eb3c0d8cbbb3e11360e847261a70af9ad
Jamie Van Dommelen <vandommelen.jamie@gmail.com>
2025-04-03 15:33:15 -0400

    deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "mini-capstone.peterxjang.com" , "localhost:5173"
+    origins "mini-capstone.peterxjang.com" , "localhost:5173", "mini-capstone-frontend-oxml.onrender.com"
     resource "*", headers: :any, credentials: true, methods: [ :get, :post, :patch, :put, :delete ]
   end
 end
